b606f776bf94cb876aa9bfd9d377e601b0c20f4d
VailexHvH <57669278+VailexHvH@users.noreply.github.com>
2022-12-13 21:18:21 +0500

    Update ww2-game-admin-commands.lua
    [+] Script looks better now.

--- ww2-game-admin-commands.lua
+++ ww2-game-admin-commands.lua
@@ -39,17 +39,17 @@ if game.PlaceId == 9329726891 then
             end
          end
       else
-for i,v in pairs(game.Players:GetPlayers()) do 
-if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
-         local ayo = {
-            [1] = game:GetService("Players")[v.Name].Character.Humanoid,
-            [2] = math.huge
-         }
+         for i,v in pairs(game.Players:GetPlayers()) do
+            if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
+               local ayo = {
+                  [1] = game:GetService("Players")[v.Name].Character.Humanoid,
+                  [2] = math.huge
+               }
 
-         game:GetService("Lighting").knifeEvent:FireServer(unpack(ayo))
+               game:GetService("Lighting").knifeEvent:FireServer(unpack(ayo))
+            end
+         end
       end
-end
-end
    end
 
    function GiveMoney(params)
@@ -69,12 +69,12 @@ end
          local getplr = game.Players:GetPlayers()[getrandom]
          game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[getplr.Name].Character.HumanoidRootPart.CFrame
       else
-for i,v in pairs(game.Players:GetPlayers()) do 
-if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
-         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[v.Name].Character.HumanoidRootPart.CFrame
+         for i,v in pairs(game.Players:GetPlayers()) do
+            if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
+               game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[v.Name].Character.HumanoidRootPart.CFrame
+            end
+         end
       end
-end
-end
    end
 
    function Godmode(params)
@@ -124,19 +124,19 @@ end
 
          game:GetService("Lighting").BuyVestEvent:FireServer(unpack(ayo))
       else
-for i,v in pairs(game.Players:GetPlayers()) do 
-if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
-         local ayo = {
-            [1] = 0,
-            [2] = "Level4vest",
-            [3] = game:GetService("Players")[v.Name].Character.Humanoid,
-            [4] = math.huge
-         }
+         for i,v in pairs(game.Players:GetPlayers()) do
+            if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
+               local ayo = {
+                  [1] = 0,
+                  [2] = "Level4vest",
+                  [3] = game:GetService("Players")[v.Name].Character.Humanoid,
+                  [4] = math.huge
+               }
 
-         game:GetService("Lighting").BuyVestEvent:FireServer(unpack(ayo))
+               game:GetService("Lighting").BuyVestEvent:FireServer(unpack(ayo))
+            end
+         end
       end
-end
-end
    end
 
    function Heal(params)
@@ -297,47 +297,47 @@ end
 
          game:GetService("Lighting").UseBandageEvent:FireServer(unpack(leftleg))
       else
-for i,v in pairs(game.Players:GetPlayers()) do 
-if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
-         local head = {
-            [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.Head,
-            [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
-         }
+         for i,v in pairs(game.Players:GetPlayers()) do
+            if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
+               local head = {
+                  [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.Head,
+                  [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
+               }
 
-         game:GetService("Lighting").UseBandageEvent:FireServer(unpack(head))
-         local torso = {
-            [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.Torso,
-            [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
-         }
+               game:GetService("Lighting").UseBandageEvent:FireServer(unpack(head))
+               local torso = {
+                  [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.Torso,
+                  [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
+               }
 
-         game:GetService("Lighting").UseBandageEvent:FireServer(unpack(torso))
-         local rightarm = {
-            [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.RightArm,
-            [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
-         }
+               game:GetService("Lighting").UseBandageEvent:FireServer(unpack(torso))
+               local rightarm = {
+                  [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.RightArm,
+                  [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
+               }
 
-         game:GetService("Lighting").UseBandageEvent:FireServer(unpack(rightarm))
-         local leftarm = {
-            [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.LeftArm,
-            [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
-         }
+               game:GetService("Lighting").UseBandageEvent:FireServer(unpack(rightarm))
+               local leftarm = {
+                  [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.LeftArm,
+                  [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
+               }
 
-         game:GetService("Lighting").UseBandageEvent:FireServer(unpack(leftarm))
-         local rightleg = {
-            [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.RightLeg,
-            [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
-         }
+               game:GetService("Lighting").UseBandageEvent:FireServer(unpack(leftarm))
+               local rightleg = {
+                  [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.RightLeg,
+                  [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
+               }
 
-         game:GetService("Lighting").UseBandageEvent:FireServer(unpack(rightleg))
-         local leftleg = {
-            [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.LeftLeg,
-            [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
-         }
+               game:GetService("Lighting").UseBandageEvent:FireServer(unpack(rightleg))
+               local leftleg = {
+                  [1] = game:GetService("Players")[v.Name].Character.CharacterHealth.LeftLeg,
+                  [2] = game:GetService("Players")[v.Name].Character.CharacterHealth
+               }
 
-         game:GetService("Lighting").UseBandageEvent:FireServer(unpack(leftleg))
+               game:GetService("Lighting").UseBandageEvent:FireServer(unpack(leftleg))
+            end
+         end
       end
-end
-end
    end
 
    function GiveHP(params)
@@ -386,19 +386,19 @@ end
             end
          end
       else
-for i,v in pairs(game.Players:GetPlayers()) do 
-if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
-         local health = {
-            [1] = 0,
-            [2] = "Level4vest",
-            [3] = game:GetService("Players")[v.Name].Character.Humanoid,
-            [4] = params[3]
-         }
+         for i,v in pairs(game.Players:GetPlayers()) do
+            if v.Name:lower():sub(1,#params[2]) == params[2]:lower() then
+               local health = {
+                  [1] = 0,
+                  [2] = "Level4vest",
+                  [3] = game:GetService("Players")[v.Name].Character.Humanoid,
+                  [4] = params[3]
+               }
 
-         game:GetService("Lighting").BuyVestEvent:FireServer(unpack(health))
+               game:GetService("Lighting").BuyVestEvent:FireServer(unpack(health))
+            end
+         end
       end
-end
-end
    end
 
    game.Players.LocalPlayer.Chatted:Connect(function(msg)
