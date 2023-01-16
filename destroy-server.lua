95a0849fe09e150509b818e2ff75075048c0984b
VailexHvH <57669278+VailexHvH@users.noreply.github.com>
2023-01-16 13:10:13 +0500

    Update destroy-server.lua

--- destroy-server.lua
+++ destroy-server.lua
@@ -1,1 +1,9 @@
-
+local properties = {
+   Color = Color3.new(12, 255, 0);
+   Font = Enum.Font.FredokaOne;
+   TextSize = 16;
+}
+wait(1)
+properties.Text = "Destroying Server..."
+game.StarterGui:SetCore("ChatMakeSystemMessage", properties)
+wait(1)
