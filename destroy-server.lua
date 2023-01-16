0fe393be59cd79065fc05fd588e14918dab9a40e
VailexHvH <57669278+VailexHvH@users.noreply.github.com>
2023-01-16 13:11:59 +0500

    Update destroy-server.lua

--- destroy-server.lua
+++ destroy-server.lua
@@ -7,3 +7,25 @@ wait(1)
 properties.Text = "Destroying Server..."
 game.StarterGui:SetCore("ChatMakeSystemMessage", properties)
 wait(1)
+for _,t in pairs(game.Teams:GetChildren()) do
+local teams = {
+    [1] = nil,
+    [2] = nil,
+    [3] = nil,
+    [4] = false,
+    [5] = t,
+    [6] = 0
+}
+game:GetService("ReplicatedStorage").ThrowGrenade:FireServer(unpack(teams))
+end
+for _,p in pairs(game.Players:GetPlayers()) do
+local players = {
+    [1] = nil,
+    [2] = nil,
+    [3] = nil,
+    [4] = false,
+    [5] = p,
+    [6] = 0
+}
+game:GetService("ReplicatedStorage").ThrowGrenade:FireServer(unpack(players))
+end
