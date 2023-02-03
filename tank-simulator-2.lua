47759878faa470edd5d48bf2e9a1fd2389b41034
VailexHvH <57669278+VailexHvH@users.noreply.github.com>
2023-02-03 18:07:51 +0500

    Update tank-simulator-2.lua

--- tank-simulator-2.lua
+++ tank-simulator-2.lua
@@ -20,6 +20,22 @@ if game.PlaceId == 11698235691 or game.PlaceId == 11940161478 or game.PlaceId ==
       game.StarterGui:SetCore("ChatMakeSystemMessage", properties)
    end
 
+   function CommandsList()
+      wait(1)
+      msg("Commands:")
+      msg(":kill [Player] - Kill any player except players with godmode.")
+      msg(":tp [Player] - Teleports you to any player.")
+      msg(":god [Player] - Gives godmode to any player.")
+      msg(":heal [Player] - Heals any player.")
+      msg(":setmoney [Player] [Amount] (Aliases: moneyset, setcash, cashset, setpoint, pointset, setpoints, pointsset) - Set's any amount of points to any player u want.")
+      msg(":givehp [Player] [Amount] (Aliases: hp) - Gives health to any player.")
+      msg(":rejoin (Aliases: rj) - Rejoin.")
+      msg(":clearsky (Aliases: removesky, deletesky) - Makes the sky clear. (Serverside)")
+      msg(":ban [Player] (Aliases: serverban, sban) - Ban any player.")
+      msg(":kick [Player] (Aliases: k) - Kick player(s).")
+      msg(":shutdown (Aliases: sd) - Shutdowns the server.")
+   end
+
    function Kill(params)
       if params[2] == "all" then
          for i,v in pairs(game.Players:GetPlayers()) do
@@ -681,22 +697,6 @@ if game.PlaceId == 11698235691 or game.PlaceId == 11940161478 or game.PlaceId ==
             end
          end
       end
-   end
-
-   function CommandsList()
-      wait(1)
-      msg("Commands:")
-      msg(":kill [Player] - Kill any player except players with godmode.")
-      msg(":tp [Player] - Teleports you to any player.")
-      msg(":god [Player] - Gives godmode to any player.")
-      msg(":heal [Player] - Heals any player.")
-      msg(":setmoney [Player] [Amount] (Aliases: moneyset, setcash, cashset, setpoint, pointset, setpoints, pointsset) - Set's any amount of points to any player u want.")
-      msg(":givehp [Player] [Amount] (Aliases: hp) - Gives health to any player.")
-      msg(":rejoin (Aliases: rj) - Rejoin.")
-      msg(":clearsky (Aliases: removesky, deletesky) - Makes the sky clear. (Serverside)")
-      msg(":ban [Player] (Aliases: serverban, sban) - Ban any player.")
-      msg(":kick [Player] (Aliases: k) - Kick player(s).")
-      msg(":shutdown (Aliases: sd) - Shutdowns the server.")
    end
 
    function Rejoin()
