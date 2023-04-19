4fe8e7f89b5b23e969b59d0697170a86f1233f20
VailexHvH <57669278+VailexHvH@users.noreply.github.com>
2023-04-20 00:58:59 +0500

    Update tank-simulator-2.lua

--- tank-simulator-2.lua
+++ tank-simulator-2.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 11698235691 or game.PlaceId == 11940161478 or game.PlaceId == 12015736082 or game.PlaceId == 12173240994 or game.PlaceId == 12335478362 then
+if game.PlaceId == 11698235691 or game.PlaceId == 11940161478 or game.PlaceId == 12015736082 or game.PlaceId == 12173240994 or game.PlaceId == 12335478362 or game.PlaceId == https:13149910801 then
    --gameplaceid start
 
    local function findplr(plr)
